14294fd15f5b677c3f7abd9965c9f3a48c34c1e7
OrcaXS Suen <sun16281394@gmail.com>
2019-03-25 18:04:11 +0800

    Add vue-router

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import LandingPage from './LandingPage';
+import PolicyPage from './PolicyPage';
+
+const routes = [
+  { path: '/', component: LandingPage },
+  { path: '/', component: PolicyPage },
+];
+
+export default routes;
