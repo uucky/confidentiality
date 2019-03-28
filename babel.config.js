6303c4223256f4195518bc7bb6fe9d9f6ea37487
OrcaXS Suen <sun16281394@gmail.com>
2019-03-28 18:55:55 +0800

    Update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   presets: [
-    '@vue/app',
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage',
+        corejs: 'corejs@3',
+      },
+    ],
   ],
+  ignore: [/\/core-js/],
 };
